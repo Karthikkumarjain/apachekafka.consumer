96d30e98cd395690293537cc0cad49a329193e86
Karthik Kumar B <karthik.kumarb@Karthiks-MacBook-Pro.local>
2023-10-07 12:39:38 +0530

    Added e2e integration test with test container

--- src/test/java/com/kk/apachekafkaconsumer/ApplicationTests.java
+++ src/test/java/com/kk/apachekafkaconsumer/ApplicationTests.java
@@ -1,13 +1,50 @@
 package com.kk.apachekafkaconsumer;
 
+import com.kk.dto.EmployeeDetails;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+import java.time.Duration;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.awaitility.Awaitility.await;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@Slf4j
 class ApplicationTests {
 
+    @Container
+    static final KafkaContainer kafka =
+            new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"));
+
+    @DynamicPropertySource
+    static void overridePropertiesInternal(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+    }
+
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+
     @Test
-    void contextLoads() {
+    public void testConsumeEvents() {
+        log.info("testConsumeEvents method execution started...");
+        EmployeeDetails employeeDetails = new EmployeeDetails(263, "Karthik", "Kumar", "TW");
+        kafkaTemplate.send("quickstart-4", employeeDetails);
+        log.info("testConsumeEvents method execution ended...");
+        await().pollInterval(Duration.ofSeconds(3)).atMost(10, SECONDS).untilAsserted(() -> {
+            //Assert
+        });
     }
 
 }
